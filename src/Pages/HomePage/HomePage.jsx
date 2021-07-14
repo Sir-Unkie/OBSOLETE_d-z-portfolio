7bdf10c05bbc1bde77e82700a610e9f0f9954489
Sir-Unkie <unknown920@gmail.com>
2021-07-14 06:37:57 +0300

    improved icons and added links for the icons

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -24,10 +24,26 @@ const HomePage = () => {
         </ul>
       </main>
       <div className={styles.iconsContainer}>
-        <LinkedIn className={styles.icon}></LinkedIn>
-        <GitHub className={styles.icon}></GitHub>
-        <WhatsApp className={styles.icon}></WhatsApp>
-        <Telegram className={styles.icon}></Telegram>
+        <a
+          href='https://www.linkedin.com/in/dmitry-zhidovlenkov-98b8a1169'
+          target='_blank'
+          rel='noreferrer'
+        >
+          <LinkedIn className={styles.icon}></LinkedIn>
+        </a>
+        <a href='https://github.com/Sir-Unkie' target='_blank' rel='noreferrer'>
+          <GitHub className={styles.icon}></GitHub>
+        </a>
+        <a href='https://wa.me/79197255841' target='_blank' rel='noreferrer'>
+          <WhatsApp className={styles.icon}></WhatsApp>
+        </a>
+        <a
+          href='https://telegram.me/Sir_Unkie'
+          target='_blank'
+          rel='noreferrer'
+        >
+          <Telegram className={styles.icon}></Telegram>
+        </a>
       </div>
       <div className='image'></div>
     </div>
