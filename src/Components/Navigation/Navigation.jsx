dcf473627feafec08d194e622670f3c66b4c00a0
Sir-Unkie <unknown920@gmail.com>
2021-07-14 06:18:23 +0300

    added basic layout to the homepage, added more colors and icons

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -7,22 +7,38 @@ const Navigation = () => {
     <nav className={styles.nav}>
       <ul className={styles.navlist}>
         <li className={styles.navitem}>
-          <NavLink to='/home' activeClassName={styles.activeNavlink}>
+          <NavLink
+            className={styles.navlink}
+            to='/home'
+            activeClassName={styles.activeNavlink}
+          >
             Home
           </NavLink>
         </li>
         <li className={styles.navitem}>
-          <NavLink to='/projects' activeClassName={styles.activeNavlink}>
+          <NavLink
+            className={styles.navlink}
+            to='/projects'
+            activeClassName={styles.activeNavlink}
+          >
             Projects
           </NavLink>
         </li>
         <li className={styles.navitem}>
-          <NavLink to='/about' activeClassName={styles.activeNavlink}>
+          <NavLink
+            className={styles.navlink}
+            to='/about'
+            activeClassName={styles.activeNavlink}
+          >
             About me
           </NavLink>
         </li>
         <li className={styles.navitem}>
-          <NavLink to='/contact' activeClassName={styles.activeNavlink}>
+          <NavLink
+            className={styles.navlink}
+            to='/contact'
+            activeClassName={styles.activeNavlink}
+          >
             Contact
           </NavLink>
         </li>
--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,8 +1,37 @@
 import React from 'react';
 import styles from './HomePage.module.scss';
+import { ReactComponent as LinkedIn } from '../../Assets/jam_linkedin-square.svg';
+import { ReactComponent as GitHub } from '../../Assets/ant-design_github-filled.svg';
+import { ReactComponent as WhatsApp } from '../../Assets/cib_whatsapp.svg';
+import { ReactComponent as Telegram } from '../../Assets/fa-brands_telegram.svg';
 
 const HomePage = () => {
-  return <div className={styles.homepage}>This is a HOMEPAGE</div>;
+  return (
+    <div className={styles.homepage}>
+      <main className='leftside'>
+        <h1 className={styles.primaryHeading}>
+          FRONT-END
+          <br />
+          WEB DEVELOPER
+        </h1>
+        <p className={styles.paragraph}>
+          I am focused on creating web applications with:
+        </p>
+        <ul className={styles.featuresList}>
+          <li className={styles.feature}>Optimized performance</li>
+          <li className={styles.feature}>Responsive layouts</li>
+          <li className={styles.feature}>Intuitive UX/UI design</li>
+        </ul>
+      </main>
+      <div className={styles.iconsContainer}>
+        <LinkedIn className={styles.icon}></LinkedIn>
+        <GitHub className={styles.icon}></GitHub>
+        <WhatsApp className={styles.icon}></WhatsApp>
+        <Telegram className={styles.icon}></Telegram>
+      </div>
+      <div className='image'></div>
+    </div>
+  );
 };
 
 export default HomePage;
