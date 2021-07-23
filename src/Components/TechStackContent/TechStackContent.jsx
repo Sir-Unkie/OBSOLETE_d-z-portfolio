9f3b3b2504a653f5e1522f7df40eb7686e092e0b
Sir-Unkie <unknown920@gmail.com>
2021-07-23 05:43:22 +0300

    some basic about me card elaboration

--- src/Components/TechStackContent/TechStackContent.jsx
+++ src/Components/TechStackContent/TechStackContent.jsx
@@ -2,7 +2,51 @@ import React from 'react';
 import styles from './TechStackContent.module.scss';
 
 const TechStackContent = () => {
-  return <div>Tech content</div>;
+  return (
+    <div className={styles.techContainer}>
+      <section className={`${styles.section}`}>
+        <h2>Frameworks:</h2>
+        <div className={styles.row}>
+          <div className={styles.list}>
+            <ul>
+              <li>React</li>
+              <li>React ecosystem (Redux, React Router)</li>
+            </ul>
+          </div>
+          <div className={styles.icons}>icons</div>
+        </div>
+      </section>
+      <section className={`${styles.section} ${styles.sectionDark}`}>
+        <h2>Programming/Markup languages:</h2>
+        <div className={styles.row}>
+          <div className={styles.list}>
+            <ul>
+              <li>Vanilla Javascript </li>
+              <li>Typescript</li>
+              <li>CSS</li>
+              <li>SASS preprocessor</li>
+              <li>HTML</li>
+            </ul>
+          </div>
+          <div className={styles.icons}>icons</div>
+        </div>
+      </section>
+      <section className={styles.section}>
+        <h2>Design:</h2>
+        <div className={styles.row}>
+          <div className={styles.list}>
+            <ul>
+              <li>Figma </li>
+              <li>Adobe XD</li>
+              <li>Adobe Photoshop</li>
+              <li>Adobe InDesign</li>
+            </ul>
+          </div>
+          <div className={styles.icons}>icons</div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default TechStackContent;
