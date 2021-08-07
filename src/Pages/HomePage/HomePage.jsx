b6d4167994ba0336a97c062eae5fe447c27cc197
Sir-Unkie <unknown920@gmail.com>
2021-08-07 15:10:03 +0300

    fixed the homepage image scaling issue

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -6,27 +6,28 @@ import IconsBar from '../../Components/IconsBar/IconsBar';
 const HomePage = () => {
   return (
     <Fragment>
-      <div className={styles.bg}></div>
-      <div className={styles.homepage}>
-        <main>
-          <h1 className={styles.primaryHeading}>
-            FRONT-END
-            <br />
-            WEB DEVELOPER
-          </h1>
-          <p className={styles.paragraph}>
-            I am focused on creating web applications with:
-          </p>
-          <ul className={styles.featuresList}>
-            <li className={styles.feature}>Optimized performance</li>
-            <li className={styles.feature}>Responsive layouts</li>
-            <li className={styles.feature}>Intuitive UX/UI design</li>
-          </ul>
-          <Link to='/projects' className={styles.projectsBtn}>
-            See my projects
-          </Link>
-          <IconsBar></IconsBar>
-        </main>
+      <div className={styles.bg}>
+        <div className={styles.homepage}>
+          <main>
+            <h1 className={styles.primaryHeading}>
+              FRONT-END
+              <br />
+              WEB DEVELOPER
+            </h1>
+            <p className={styles.paragraph}>
+              I am focused on creating web applications with:
+            </p>
+            <ul className={styles.featuresList}>
+              <li className={styles.feature}>Optimized performance</li>
+              <li className={styles.feature}>Responsive layouts</li>
+              <li className={styles.feature}>Intuitive UX/UI design</li>
+            </ul>
+            <Link to='/projects' className={styles.projectsBtn}>
+              See my projects
+            </Link>
+            <IconsBar></IconsBar>
+          </main>
+        </div>
       </div>
     </Fragment>
   );
