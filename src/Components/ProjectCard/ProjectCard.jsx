86cef0796a514de3d34514fa1012d0348b0a2056
Sir-Unkie <unknown920@gmail.com>
2021-08-10 00:49:00 +0300

    changed the project card font size and layout

--- src/Components/ProjectCard/ProjectCard.jsx
+++ src/Components/ProjectCard/ProjectCard.jsx
@@ -29,23 +29,25 @@ const ProjectCard = ({
         ></div>
       </div>
       <div className={styles.content}>
-        <h2 className={styles.projectName}>{projectName}</h2>
-        <div className={styles.listsContainer}>
-          <div className={styles.technologyStack}>
-            <p className={styles.paragraph}>Technology stack:</p>
-            <ul className={styles.list}>
-              {technologyStack.map(elem => {
-                return <li className={styles.listItem}>{elem}</li>;
-              })}
-            </ul>
-          </div>
-          <div className={styles.mainFeatures}>
-            <p className={styles.paragraph}>Main Features:</p>
-            <ul className={styles.list}>
-              {mainFeatures.map(feature => {
-                return <li className={styles.listItem}>{feature}</li>;
-              })}
-            </ul>
+        <div>
+          <h2 className={styles.projectName}>{projectName}</h2>
+          <div className={styles.listsContainer}>
+            <div className={styles.technologyStack}>
+              <p className={styles.paragraph}>Technology stack:</p>
+              <ul className={styles.list}>
+                {technologyStack.map(elem => {
+                  return <li className={styles.listItem}>{elem}</li>;
+                })}
+              </ul>
+            </div>
+            <div className={styles.mainFeatures}>
+              <p className={styles.paragraph}>Main Features:</p>
+              <ul className={styles.list}>
+                {mainFeatures.map(feature => {
+                  return <li className={styles.listItem}>{feature}</li>;
+                })}
+              </ul>
+            </div>
           </div>
         </div>
         <div className={styles.btnContainer}>
