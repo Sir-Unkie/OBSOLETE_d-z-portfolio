39eddcee84c9a640706231acce8019f9b3acc50a
Sir-Unkie <unknown920@gmail.com>
2021-08-17 19:57:47 +0300

    added hover me button on contact page

--- src/Pages/ContactPage/Copy.jsx
+++ src/Pages/ContactPage/Copy.jsx
@@ -18,51 +18,53 @@ const ContactPage = () => {
       <div className={styles.pageBorderOverflowContainer}>
         <div className={styles.contactPage}>
           <MyHeading>Contact me</MyHeading>
-          <div className={styles.leftArea}>
-            <h2>Have any questions or an interesting project towork on?</h2>
-            <p className={styles.paragraph}>
-              I'd love to hear from you!<br></br>
-              Just fill up the form or send an email at&nbsp;
-              <a
-                href='mailto:zhidovlenkovDI@gmail.com'
-                className={styles.colored}
-              >
-                ZhidovlenkovDI@gmail.com
-              </a>
-            </p>
-            <CustomForm></CustomForm>
-          </div>
-        </div>
-        <div className={styles.rightArea}>
-          <div className={styles.github}>
-            <ContactButton
-              goToLink='https://github.com/Sir-Unkie'
-              icon='GitHub'
-            ></ContactButton>
-          </div>
-          <div className={styles.mail}>
-            <ContactButton
-              goToLink='mailto:zhidovlenkovDI@gmail.com'
-              icon='Mail'
-            ></ContactButton>
-          </div>
-          <div className={styles.whatsapp}>
-            <ContactButton
-              goToLink='https://wa.me/79197255841'
-              icon='WhatsApp'
-            ></ContactButton>
-          </div>
-          <div className={styles.telegram}>
-            <ContactButton
-              goToLink='https://telegram.me/Sir_Unkie'
-              icon='Telegram'
-            ></ContactButton>
-          </div>
-          <div className={styles.linkedIn}>
-            <ContactButton
-              goToLink='https://www.linkedin.com/in/dmitry-zhidovlenkov'
-              icon='LinkedIn'
-            ></ContactButton>
+ 
+            <div className={styles.leftArea}>
+              <h2>Have any questions or an interesting project towork on?</h2>
+              <p className={styles.paragraph}>
+                I'd love to hear from you!<br></br>
+                Just fill up the form or send an email at&nbsp;
+                <a
+                  href='mailto:zhidovlenkovDI@gmail.com'
+                  className={styles.colored}
+                >
+                  ZhidovlenkovDI@gmail.com
+                </a>
+              </p>
+              <CustomForm></CustomForm>
+            </div>
+            <div className={styles.rightArea}>
+              <div className={styles.github}>
+                <ContactButton
+                  goToLink='https://github.com/Sir-Unkie'
+                  icon='GitHub'
+                ></ContactButton>
+              </div>
+              <div className={styles.mail}>
+                <ContactButton
+                  goToLink='mailto:zhidovlenkovDI@gmail.com'
+                  icon='Mail'
+                ></ContactButton>
+              </div>
+              <div className={styles.whatsapp}>
+                <ContactButton
+                  goToLink='https://wa.me/79197255841'
+                  icon='WhatsApp'
+                ></ContactButton>
+              </div>
+              <div className={styles.telegram}>
+                <ContactButton
+                  goToLink='https://telegram.me/Sir_Unkie'
+                  icon='Telegram'
+                ></ContactButton>
+              </div>
+              <div className={styles.linkedIn}>
+                <ContactButton
+                  goToLink='https://www.linkedin.com/in/dmitry-zhidovlenkov'
+                  icon='LinkedIn'
+                ></ContactButton>
+              </div>
+            </div>
           </div>
         </div>
       </div>
